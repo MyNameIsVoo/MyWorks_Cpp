9f87460b776e8cf0df5aa1f5783a933d7881000f
Семен <choco.16mail@mail.ru>
2023-12-19 10:02:56 +0300

    Добавлен Симплекс-Метод линейного программирования (поиск максимума функции)

--- MyWorks_Cpp/MyWorks_Cpp.cpp
+++ MyWorks_Cpp/MyWorks_Cpp.cpp
@@ -1,6 +1,35 @@
-﻿#include <iostream>
+﻿#include "SimplexMethod.h"
 
-int main()
+int main() 
 {
-    std::cout << "Hello World!\n";
+	setlocale(LC_ALL, "Russian");
+
+	int canExit = 0;
+	do
+	{
+		int index = -1;
+		do
+		{
+			cout << "\t\tВведите желаемый метод\n\n";
+			cout << "\t\t0 - Симплекс-метод линейного программирования (поиск максимума)\n\n";
+			cin >> index;
+			if (index < 0 || index > 0)
+				cout << "\t\tВы ввели некорректное число\n\n";
+			else
+				break;
+		} while (true);
+
+		cout << "\t\tВы выбрали метод под номером" << index << endl << endl;
+
+		if (index == 0)
+			InitSimplexMethod();
+
+		cout << "\n\n\n\t\tПродолжить? 0 - Да, 1 - Нет" << endl << endl;
+		cin >> canExit;
+		if (canExit != 0)
+			break;
+
+	} while (true);
+
+	return 0;
 }
